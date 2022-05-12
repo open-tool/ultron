6909b18b67c7570c468f8da36c6e70e2a13d0f32
n.shevtsov <n.shevtsov@corp.vk.com>
2022-05-12 15:43:51 +0500

    split 1 test to 8

--- sample-app/src/androidTest/java/com/atiurin/sampleapp/tests/espresso/CustomClicksTest.kt
+++ sample-app/src/androidTest/java/com/atiurin/sampleapp/tests/espresso/CustomClicksTest.kt
@@ -9,7 +9,7 @@ import com.atiurin.ultron.extensions.*
 import com.atiurin.ultron.testlifecycle.setupteardown.SetUpRule
 import org.junit.Test
 
-class CustomClicksTest: BaseTest() {
+class CustomClicksTest : BaseTest() {
 
     private val startActivity = SetUpRule().add {
         ActivityScenario.launch(CustomClicksActivity::class.java)
@@ -23,26 +23,47 @@ class CustomClicksTest: BaseTest() {
     fun clickTopLeft() {
         withId(R.id.imageView).clickTopLeft(offsetX = 30, offsetY = 30)
         withId(R.id.rB_top_left).isChecked()
+    }
 
+    @Test
+    fun clickTopCenter() {
         withId(R.id.imageView).clickTopCenter(offsetY = 30)
         withId(R.id.rB_top_center).isChecked()
+    }
 
+    @Test
+    fun clickTopRight() {
         withId(R.id.imageView).clickTopRight(offsetX = -30, offsetY = 30)
         withId(R.id.rB_top_right).isChecked()
+    }
 
+    @Test
+    fun clickCenterRight() {
         withId(R.id.imageView).clickCenterRight(offsetX = -30)
         withId(R.id.rB_center_right).isChecked()
+    }
 
+    @Test
+    fun clickBottomRight() {
         withId(R.id.imageView).clickBottomRight(offsetX = -30, offsetY = -30)
         withId(R.id.rB_bottom_right).isChecked()
+    }
 
+    @Test
+    fun clickBottomCenter() {
         withId(R.id.imageView).clickBottomCenter(offsetY = -30)
         withId(R.id.rB_bottom_center).isChecked()
+    }
 
+    @Test
+    fun clickBottomLeft() {
         withId(R.id.imageView).clickBottomLeft(offsetX = 30, offsetY = -30)
         withId(R.id.rB_bottom_left).isChecked()
+    }
 
+    @Test
+    fun clickCenterLeft() {
         withId(R.id.imageView).clickCenterLeft(offsetX = 30)
-        withId(R.id.rB_center_right).isChecked()
+        withId(R.id.rB_center_left).isChecked()
     }
 }
