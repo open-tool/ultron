6e3fc24005bcf5f9a6c12c393a3e7e0869817824
Aleksei Tiurin <alexeytyurin74@gmail.com>
2022-12-12 01:48:50 +0300

    Simplify framework extension

--- sample-app/src/androidTest/java/com/atiurin/sampleapp/framework/ultronext/UltronEspressoWebExt.kt
+++ sample-app/src/androidTest/java/com/atiurin/sampleapp/framework/ultronext/UltronEspressoWebExt.kt
@@ -1,24 +1,18 @@
 package com.atiurin.sampleapp.framework.ultronext
 
-import androidx.test.espresso.web.model.Evaluation
 import androidx.test.espresso.web.webdriver.DriverAtoms
-import com.atiurin.ultron.core.config.UltronConfig
 import com.atiurin.ultron.core.espressoweb.operation.EspressoWebOperationType
-import com.atiurin.ultron.core.espressoweb.operation.WebInteractionOperation
-import com.atiurin.ultron.core.espressoweb.operation.WebOperationResult
-import com.atiurin.ultron.core.espressoweb.webelement.UltronWebDocument
 import com.atiurin.ultron.core.espressoweb.webelement.UltronWebElement
 
 // add action on wenView
 fun UltronWebElement.appendText(text: String) = apply {
-    UltronWebDocument.executeOperation(
-        webInteractionBlock = {
-            webInteractionBlock().perform(DriverAtoms.webKeys(text))
-        },
-        name = " WebElement(${locator.type} = '$value') ReplaceText to '$text'",
-        type = EspressoWebOperationType.WEB_REPLACE_TEXT,
-        description = " WebElement(${locator.type} = '$value') ReplaceText to '$text' during $timeoutMs ms",
-        timeoutMs = timeoutMs ?: UltronConfig.Espresso.ACTION_TIMEOUT,
-        resultHandler as (WebOperationResult<WebInteractionOperation<Evaluation>>) -> Unit
+    executeOperation(
+        getUltronWebActionOperation(
+            webInteractionBlock = {
+                webInteractionBlock().perform(DriverAtoms.webKeys(text))
+            },
+            name = "WebElement(${locator.type} = '$value') appendText '$text'",
+            description = "WebElement(${locator.type} = '$value') appendText '$text' during $timeoutMs ms"
+        )
     )
 }
