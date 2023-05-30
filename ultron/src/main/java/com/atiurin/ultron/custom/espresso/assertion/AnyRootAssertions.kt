2f718c14bb066ca8f96cf584a62f99a04d6909bf
Sergey Potapov <itsergpot@gmail.com>
2023-05-30 23:02:57 +0300

    fix doesNotExistInAnyVisibleRoot tests, logic

--- ultron/src/main/java/com/atiurin/ultron/custom/espresso/assertion/AnyRootAssertions.kt
+++ ultron/src/main/java/com/atiurin/ultron/custom/espresso/assertion/AnyRootAssertions.kt
@@ -5,29 +5,27 @@ import androidx.test.espresso.DataInteraction
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import com.atiurin.ultron.core.espresso.UltronEspressoInteraction
 import com.atiurin.ultron.core.espresso.assertion.EspressoAssertionType
-import com.atiurin.ultron.custom.espresso.base.getRootViewsList
-import org.hamcrest.CoreMatchers.*
+import com.atiurin.ultron.custom.espresso.base.getVisibleRootViews
+import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.Matcher
 
-fun <T> UltronEspressoInteraction<T>.doesNotExistInAnyRoot() = apply {
-    executeAssertion(
-        operationBlock = {
-            getRootViewsList().forEach { root ->
-                this@doesNotExistInAnyRoot.apply {
-                    inRoot(RootMatchers.withDecorView(`is`(root.decorView)))
-                    getInteractionAssertionBlock(ViewAssertions.doesNotExist())
-                }
-            }
-        },
-        name = "DoesNotExist of '${getInteractionMatcher()}'",
-        type = EspressoAssertionType.DOES_NOT_EXIST,
-        description = "${interaction.className()} assertion '${EspressoAssertionType.DOES_NOT_EXIST}' of '${getInteractionMatcher()}' with root '${getInteractionRootMatcher()}' during ${getAssertionTimeout()} ms"
-    )
+fun <T> UltronEspressoInteraction<T>.doesNotExistInAnyVisibleRoot() : UltronEspressoInteraction<T> {
+    val defaultUltronEspressoInteraction = this
+    getVisibleRootViews().forEach { root ->
+        this.inRoot(withDecorView(`is`(root.decorView)))
+        executeAssertion(
+            operationBlock = getInteractionAssertionBlock(ViewAssertions.doesNotExist()),
+            name = "DoesNotExist of '${getInteractionMatcher()}'",
+            type = EspressoAssertionType.DOES_NOT_EXIST,
+            description = "${interaction.className()} assertion '${EspressoAssertionType.DOES_NOT_EXIST}' of '${getInteractionMatcher()}' with root '${getInteractionRootMatcher()}' during ${getAssertionTimeout()} ms"
+        )
+    }
+    return defaultUltronEspressoInteraction
 }
 
-fun Matcher<View>.doesNotExistInAnyRoot() = UltronEspressoInteraction(onView(this)).doesNotExistInAnyRoot()
-fun ViewInteraction.doesNotExistInAnyRoot() = UltronEspressoInteraction(this).doesNotExistInAnyRoot()
-fun DataInteraction.doesNotExistInAnyRoot() = UltronEspressoInteraction(this).doesNotExistInAnyRoot()
+fun Matcher<View>.doesNotExistInAnyVisibleRoot() = UltronEspressoInteraction(onView(this)).doesNotExistInAnyVisibleRoot()
+fun ViewInteraction.doesNotExistInAnyVisibleRoot() = UltronEspressoInteraction(this).doesNotExistInAnyVisibleRoot()
+fun DataInteraction.doesNotExistInAnyVisibleRoot() = UltronEspressoInteraction(this).doesNotExistInAnyVisibleRoot()
