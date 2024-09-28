73c93750edd9388f8cdce6e0dfc762f5d5209339
Aleksei Tiurin <alexeytyurin74@gmail.com>
2024-09-28 23:57:09 +0300

    Fix allure step with assertion (#91)

--- ultron-allure/src/main/java/com/atiurin/ultron/allure/listeners/DetailedOperationAllureListener.kt
+++ ultron-allure/src/main/java/com/atiurin/ultron/allure/listeners/DetailedOperationAllureListener.kt
@@ -2,18 +2,20 @@ package com.atiurin.ultron.allure.listeners
 
 import com.atiurin.ultron.core.common.Operation
 import com.atiurin.ultron.core.common.OperationResult
+import com.atiurin.ultron.exceptions.UltronException
 import com.atiurin.ultron.listeners.UltronLifecycleListener
 import io.qameta.allure.kotlin.Allure
 import io.qameta.allure.kotlin.model.Status
 import io.qameta.allure.kotlin.model.StepResult
 import io.qameta.allure.kotlin.util.ResultsUtils
-import java.util.*
+import java.util.UUID
 
 class DetailedOperationAllureListener : UltronLifecycleListener() {
-    lateinit var stepName: String
-    lateinit var uuid: String
+    private val stepsMap = mutableMapOf<Operation, String>()
+
     override fun after(operationResult: OperationResult<Operation>) {
-        Allure.lifecycle.stopStep(uuid)
+        Allure.lifecycle.stopStep(stepsMap.getUuid(operationResult.operation))
+        stepsMap.remove(operationResult.operation)
     }
 
     override fun afterFailure(operationResult: OperationResult<Operation>) {
@@ -27,14 +29,21 @@ class DetailedOperationAllureListener : UltronLifecycleListener() {
     }
 
     override fun afterSuccess(operationResult: OperationResult<Operation>) {
-        Allure.lifecycle.updateStep(uuid) { it.status = Status.PASSED }
+        Allure.lifecycle.updateStep(stepsMap.getUuid(operationResult.operation)) {
+            it.status = Status.PASSED
+        }
     }
 
     override fun before(operation: Operation) {
-        uuid = UUID.randomUUID().toString()
-        stepName = operation.name
+        val uuid = UUID.randomUUID().toString()
+        stepsMap[operation] = uuid
+        val stepName = operation.name
         Allure.lifecycle.startStep(uuid, StepResult().apply {
             this.name = stepName
         })
+    }
+
+    private fun Map<Operation, String>.getUuid(operation: Operation): String {
+        return this[operation] ?: throw UltronException("No Allure UUID defined for operation ${operation.name}")
     }
 }
