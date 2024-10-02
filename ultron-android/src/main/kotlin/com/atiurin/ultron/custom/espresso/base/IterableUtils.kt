51eb1d3d5a86f320646e5c14ddceead803aa2f3f
so.potapov <so.potapov@vk.team>
2024-10-02 08:26:49 +0300

    fix comments

--- ultron-android/src/main/kotlin/com/atiurin/ultron/custom/espresso/base/IterableUtils.kt
+++ ultron-android/src/main/kotlin/com/atiurin/ultron/custom/espresso/base/IterableUtils.kt
@@ -2,19 +2,19 @@ package com.atiurin.ultron.custom.espresso.base
 
 import org.hamcrest.Matcher
 
-fun <T> filter(iterable: Iterable<T>, matcher: Matcher<T>): Iterable<T> {
+internal fun <T> filter(iterable: Iterable<T>, matcher: Matcher<T>): Iterable<T> {
     return iterable.filter { matcher.matches(it) }
 }
 
-fun <T> filterToList(iterable: Iterable<T>, matcher: Matcher<T>): List<T> {
+internal fun <T> filterToList(iterable: Iterable<T>, matcher: Matcher<T>): List<T> {
     return filter(iterable, matcher).toList()
 }
 
-fun joinToString(iterable: Iterable<Any>, delimiter: String): String {
+internal fun joinToString(iterable: Iterable<Any>, delimiter: String): String {
     return iterable.joinToString(separator = delimiter)
 }
 
-fun <T> toArray(iterator: Iterator<T>, clazz: Class<T>): Array<T> {
+internal fun <T> toArray(iterator: Iterator<T>, clazz: Class<T>): Array<T> {
     val arrayList = ArrayList<T>()
     while (iterator.hasNext()) {
         arrayList.add(iterator.next())
